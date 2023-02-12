fb6c7dc0afc5c2ca945120c357c223ae68d5345b
Xierumeng <xierumeng@hotmail.com>
2023-02-12 11:55:16 -0800

    Telemetry lint (#2)
    * gitignore
    
    * Fix and return

--- py/test_tx.py
+++ py/test_tx.py
@@ -1,19 +1,27 @@
+"""
+Test communication physically
+"""
+
 from generic_comms_device import GenericCommsDevice
 import TelemMessages
 
+
 if __name__ == "__main__":
-    sender = GenericCommsDevice('/dev/ttyUSB0', 115200) 
+    sender = GenericCommsDevice('/dev/ttyUSB0', 115200)
     receiver = GenericCommsDevice('/dev/ttyUSB1', 115200)
     index = 0
     msg = TelemMessages.GroundStationDisarm()
     msg.arm = True
     sender.transmit(msg)
     while True:
-        out = receiver.receive()
-        if out != None:
-            print(type(out))
-            print(index)
-            index  = index + 1
-            msg = TelemMessages.GroundStationDisarm()
-            msg.arm = True
-            sender.transmit(msg)
+        result, out = receiver.receive()
+        if not result:
+            continue
+
+        print(type(out))
+        print(out.header.type)
+        print(index)
+        index += 1
+        msg = TelemMessages.GroundStationDisarm()
+        msg.arm = True
+        sender.transmit(msg)
