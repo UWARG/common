12624da934596bbb09d5a24d89fff627ff6a9f97
Xierumeng <xierumeng@hotmail.com>
2023-02-15 13:26:15 -0500

    Working after test

--- test_tx.py
+++ test_tx.py
@@ -3,25 +3,22 @@ Test communication physically
 """
 
 from py.generic_comms_device import GenericCommsDevice
-import TelemMessages
+from py import TelemMessages
 
 
 if __name__ == "__main__":
-    sender = GenericCommsDevice('/dev/ttyUSB0', 115200)
-    receiver = GenericCommsDevice('/dev/ttyUSB1', 115200)
-    index = 0
-    msg = TelemMessages.GroundStationDisarm()
-    msg.arm = True
-    sender.transmit(msg)
-    while True:
+    sender = GenericCommsDevice("/dev/ttyUSB0", 115200)
+    receiver = GenericCommsDevice("/dev/ttyUSB0", 115200)
+    for i in range(0, 1000):
+        msg = TelemMessages.GroundStationDisarm()
+        msg.arm = i % 2 == 0
+        sender.transmit(msg)
+
         result, out = receiver.receive()
         if not result:
             continue
 
         print(type(out))
         print(out.header.type)
-        print(index)
-        index += 1
-        msg = TelemMessages.GroundStationDisarm()
-        msg.arm = True
-        sender.transmit(msg)
+        print(i)
+        print(out.arm)
