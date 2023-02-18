d1d24fce07de0ab93d4fe4cbe05a906f4e3be5cb
Xierumeng <xierumeng@hotmail.com>
2023-02-18 00:07:59 -0500

    Separate TX/RX working

--- comms/receive.py
+++ comms/receive.py
@@ -1,6 +1,8 @@
 """
-Test communication physically
+Run transmit.py and receive.py together
 """
+
+import time
 
 from modules.generic_comms_device import GenericCommsDevice
 
@@ -8,11 +10,12 @@ from modules.generic_comms_device import GenericCommsDevice
 if __name__ == "__main__":
     receiver = GenericCommsDevice("/dev/ttyUSB1", 115200)
     while True:
+        time.sleep(0.01)
+
         result, out = receiver.receive()
         if not result:
-            # print("false")
+            print("Failure")
             continue
 
         print(type(out))
         print(out.header.type)
-        print(out.arm)
--- comms/transmit.py
+++ comms/transmit.py
@@ -1,20 +1,19 @@
-from modules.generic_comms_device import GenericCommsDevice
-from modules import TelemMessages
+"""
+Run transmit.py and receive.py together
+"""
+
 import time
+
+from modules.generic_comms_device import GenericCommsDevice
+from modules import helper
 
 
 if __name__ == "__main__":
     sender = GenericCommsDevice("/dev/ttyUSB0", 115200)
-    for i in range(0, 1):
-        # msg = TelemMessages.GroundStationData()
-        msg = TelemMessages.GroundStationDisarm()
-        # msg.data.altitude = 5.0
-        # print("length of batt array", len(msg.battery_voltages))
-        # msg.arm = i % 2 == 0
+    for i in range(0, 1000):
+        msg = helper.message_picker(i)
         sender.transmit(msg)
-        msg_crc32 = int.from_bytes(msg.crc, 'big')
-        print(msg_crc32)
-        time.sleep(1)
+
+        time.sleep(0.01)
 
         print(i)
-        # print(msg.arm)
