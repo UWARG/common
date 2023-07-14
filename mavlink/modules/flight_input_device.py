d6ea0718f4e869293d72b04ea9cefee3e408839c
Ethan Woo <ethan.cpw.woo@outlook.com>
2023-07-13 21:35:03 -0400

    implemented drone odometry and refactored data structures

--- mavlink/modules/flight_input_device.py
+++ mavlink/modules/flight_input_device.py
@@ -1,9 +1,10 @@
-from dronekit import connect 
-
 """
 Creates a FlightInputDevice using dronekit which abstracts MavLink data
 Formats odometry data to dictionary 
 """
+
+import dronekit
+import modules.drone_odometry as drone_odometry
 
 class FlightInputDevice: 
     """
@@ -15,31 +16,26 @@ class FlightInputDevice:
         Connection address: tcp address (e.g. "tcp:127.0.0.1:14550")
         Establishes address to host and stores inside instance self.drone
         """
-        self.drone = connect(address, wait_ready = True)
+        self.drone = dronekit.connect(address, wait_ready = True)
         assert self.drone is not None
 
-    def get_data(self) -> "dict": 
+    def get_data(self) -> "drone_odometry.DroneOdometry": 
         """
         For now since the only output is to odometry worker,
         will only get odometry data.
 
         Returns odometry data in dictionary from the drone. 
         """
-        data = {}
+
         attitude_info = self.drone.attitude
-        #in radians 
-        data['pitch'] = attitude_info.pitch
-        data['yaw'] = attitude_info.yaw
-        data['roll'] = attitude_info.roll 
+        recieved_orientation, orientation_data = drone_odometry.DroneOrientation.create(attitude_info.yaw, attitude_info.pitch, attitude_info.roll)
         
         location_info = self.drone.location
-        data['alt'] = location_info.global_frame.alt
-        data['lat'] = location_info.global_frame.lat
-        data['lon'] = location_info.global_frame.lon
+        recieved_position, position_data = drone_odometry.DronePosition.create(location_info.global_frame.lat, location_info.global_frame.lon, location_info.global_frame.alt)
 
-        #if needed, distance from range finder can be added as well
+        recieved_odometry, odometry_data = drone_odometry.DroneOdometry.create(position_data, orientation_data)
 
-        if not data:
+        if not recieved_odometry:
             return None
 
-        return data
+        return odometry_data
--- mavlink/test_flight_input.py
+++ mavlink/test_flight_input.py
@@ -1,6 +1,14 @@
-from modules.flight_input_device import InputDevice
+import modules.flight_input_device 
+import modules.drone_odometry as drone_odometry
+
 
 if __name__ == "__main__":
-    example_drone = InputDevice('tcp:127.0.0.1:14550')
-    print(example_drone.get_data()) 
+    example_drone = modules.flight_input_device.FlightInputDevice('tcp:127.0.0.1:14550')
+    data = example_drone.get_data()
+    print("lat: " + str(data.position.latitude))
+    print("lon: " + str(data.position.longitude))
+    print("alt: " + str(data.position.altitude))
+    print("yaw: " + str(data.orientation.yaw))
+    print("roll: " + str(data.orientation.roll))
+    print("pitch: " + str(data.orientation.pitch))
 
