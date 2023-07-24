7dadeb7a61fc3d919930447b11aadc0939dfe97e
Ethan Woo <ethan.cpw.woo@outlook.com>
2023-07-24 12:18:29 -0400

    check for results in flight input

--- mavlink/modules/flight_input_device.py
+++ mavlink/modules/flight_input_device.py
@@ -16,15 +16,15 @@ class FlightInputDevice:
     @classmethod
     def create(cls, address: str) -> "tuple[bool, FlightInputDevice | None]":
         """
-        Connection address: tcp address or serial port (e.g. "tcp:127.0.0.1:14550").
-        Establishes address to host and stores inside instance self.drone.
+        address: tcp address or serial port of the drone (e.g. "tcp:127.0.0.1:14550").
+        Establishes address to drone and stores dronekit object inside instance drone.
         """
         drone = dronekit.connect(address, wait_ready = True)
+        if drone is None:
+            return False, None
+        return True, FlightInputDevice(cls.__create_key, drone)
         
-        if drone is not None:
-            return True, FlightInputDevice(cls.__create_key, drone) 
-        
-    def __init__(self, class_private_create_key, vehicle):
+    def __init__(self, class_private_create_key, vehicle: dronekit.Vehicle):
         """
         Private constructor, use create() method.
         """
@@ -32,27 +32,20 @@ class FlightInputDevice:
         
         self.drone = vehicle
 
-    def get_data(self) -> "tuple[bool, drone_odometry.DroneOdometry | None]":
+    def get_odometry(self) -> "tuple[bool, drone_odometry.DroneOdometry | None]":
         """
-        For now since the only output is to odometry worker, will only get odometry data.
-        Returns odometry data in dictionary from the drone or None if missing either orientation or position.
+        Returns odometry data as an instance of DroneOdometry from the drone or None if missing either orientation or position.
         """
         attitude_info = self.drone.attitude
         result, orientation_data = drone_odometry.DroneOrientation.create(attitude_info.yaw, attitude_info.pitch, attitude_info.roll)
-        
         if not result:
             return False, None
-
         location_info = self.drone.location
         result, position_data = drone_odometry.DronePosition.create(location_info.global_frame.lat, location_info.global_frame.lon, location_info.global_frame.alt)
-
         if not result:
             return False, None
-
         result, odometry_data = drone_odometry.DroneOdometry.create(position_data, orientation_data)
-
         if not result:
             return False, None
-
         return True, odometry_data
     
--- mavlink/test_flight_input.py
+++ mavlink/test_flight_input.py
@@ -1,22 +1,31 @@
 """
 Test for flight input device by printing to console.
 """
+import time
 
 from modules import flight_input_device
-from time import sleep
 
 
+DELAY_TIME = 0.5
+TCP_ADDRESS = 'tcp:127.0.0.1:14550'
 if __name__ == "__main__":
-    example_drone = flight_input_device.FlightInputDevice.create('tcp:127.0.0.1:14550')
-    
+    result, example_drone = flight_input_device.FlightInputDevice.create(TCP_ADDRESS)
+    if not result: 
+        print("failed")
+        quit()
+
     for i in range(5):
-        data = example_drone[1].get_data()
-        print("lat: " + str(data[1].position.latitude))
-        print("lon: " + str(data[1].position.longitude))
-        print("alt: " + str(data[1].position.altitude))
-        print("yaw: " + str(data[1].orientation.yaw))
-        print("roll: " + str(data[1].orientation.roll))
-        print("pitch: " + str(data[1].orientation.pitch) + "\n")
-        sleep(0.5)
+        result, data = example_drone.get_odometry()
+        if result:
+            print("lat:" + " " + str(data.position.latitude))
+            print("lon:" + " " + str(data.position.longitude))
+            print("alt:" + " " + str(data.position.altitude))
+            print("yaw:" + " " + str(data.orientation.yaw))
+            print("roll:" + " " + str(data.orientation.roll))
+            print("pitch:" + " " + str(data.orientation.pitch))
+            print("")
+        else: 
+            print("failed")
+        time.sleep(DELAY_TIME)
 
     print("Done!")
