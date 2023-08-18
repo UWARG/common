bbeec7fae483ebce0efe9f897be7036b5a308cbb
Xierumeng <xierumeng@hotmail.com>
2023-08-18 01:10:11 -0400

    Test working

--- mavlink/test_flight_controller.py
+++ mavlink/test_flight_controller.py
@@ -1,34 +1,45 @@
 """
 Test for flight input device by printing to console.
 """
+import sys
 import time
 
-from modules import flight_input_device
+from modules import flight_controller
 
 
 DELAY_TIME = 0.5  # seconds
 DRONE_ADDRESS = "tcp:127.0.0.1:14550"
+TIMEOUT = 1.0  # seconds
 
 
 if __name__ == "__main__":
-    result, device = flight_input_device.FlightInputDevice.create(TCP_ADDRESS)
-    if not result: 
+    result, controller = flight_controller.FlightController.create(DRONE_ADDRESS)
+    if not result:
         print("failed")
-        quit()
+        sys.exit()
+
+    # Get Pylance to stop complaining
+    assert controller is not None
 
     for i in range(5):
-        result, odometry = device.get_odometry()
+        result, odometry = controller.get_odometry()
         if result:
-            print("lat:" + " " + str(odometry.position.latitude))
-            print("lon:" + " " + str(odometry.position.longitude))
-            print("alt:" + " " + str(odometry.position.altitude))
-            print("yaw:" + " " + str(odometry.orientation.yaw))
-            print("roll:" + " " + str(odometry.orientation.roll))
-            print("pitch:" + " " + str(odometry.orientation.pitch))
+            print("lat: " + str(odometry.position.latitude))
+            print("lon: " + str(odometry.position.longitude))
+            print("alt: " + str(odometry.position.altitude))
+            print("yaw: " + str(odometry.orientation.yaw))
+            print("roll: " + str(odometry.orientation.roll))
+            print("pitch: " + str(odometry.orientation.pitch))
             print("")
-        else: 
+        else:
             print("failed")
-            
+
+        result, home = controller.get_home_location(TIMEOUT)
+        if result:
+            print("lat: " + str(home.latitude))
+            print("lon: " + str(home.longitude))
+            print("alt: " + str(home.altitude))
+
         time.sleep(DELAY_TIME)
 
     print("Done!")
