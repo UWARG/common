d302c491fc0378abb992ffc6cb4d57f8ba996c41
Xierumeng <xierumeng@hotmail.com>
2023-08-19 00:14:03 -0400

    Support for getting the drone's home location (#13)
    * Rename file
    
    * Formatting and renaming
    
    * Actual code
    
    * More renaming
    
    * Test working
    
    * Clarify test connection address

---
+++ mavlink/modules/flight_controller.py
@@ -0,0 +1,97 @@
+"""
+Wrapper for the flight controller.
+"""
+import time
+
+import dronekit
+
+from modules import drone_odometry
+
+
+class FlightController:
+    """
+    Wrapper for DroneKit-Python and MAVLink.
+    """
+    __create_key = object()
+
+    @classmethod
+    def create(cls, address: str) -> "tuple[bool, FlightController | None]":
+        """
+        address: TCP address or serial port of the drone (e.g. "tcp:127.0.0.1:14550").
+        Establishes connection to drone through provided address
+        and stores the DroneKit object.
+        """
+        try:
+            drone = dronekit.connect(address, wait_ready=True)
+        except dronekit.TimeoutError:
+            print("No messages are being received. Make sure address/port is a host address/port.")
+            return False, None
+        except ConnectionRefusedError:
+            print("Cannot connect to drone! Make sure the address/port is correct.")
+            return False, None
+
+        return True, FlightController(cls.__create_key, drone)
+
+    def __init__(self, class_private_create_key, vehicle: dronekit.Vehicle):
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is FlightController.__create_key, "Use create() method"
+
+        self.drone = vehicle
+
+    def get_odometry(self) -> "tuple[bool, drone_odometry.DroneOdometry | None]":
+        """
+        Returns odometry data from the drone.
+        """
+        attitude_info = self.drone.attitude
+        result, orientation_data = drone_odometry.DroneOrientation.create(
+            attitude_info.yaw,
+            attitude_info.pitch,
+            attitude_info.roll,
+        )
+        if not result:
+            return False, None
+
+        location_info = self.drone.location
+        result, position_data = drone_odometry.DronePosition.create(
+            location_info.global_frame.lat,
+            location_info.global_frame.lon,
+            location_info.global_frame.alt,
+        )
+        if not result:
+            return False, None
+
+        result, odometry_data = drone_odometry.DroneOdometry.create(
+            position_data,
+            orientation_data,
+        )
+        if not result:
+            return False, None
+
+        return True, odometry_data
+
+    def get_home_location(self, timeout: float) -> "tuple[bool, drone_odometry.DroneOdometry]":
+        """
+        Attempts to get the drone's home location until timeout.
+        timeout: Seconds.
+        """
+        start_time = time.time()
+        while self.drone.home_location is None and time.time() - start_time < timeout:
+            commands = self.drone.commands
+            commands.download()
+            commands.wait_ready()
+
+        # Timeout
+        if self.drone.home_location is None:
+            return False, None
+
+        result, location = drone_odometry.DronePosition.create(
+            self.drone.home_location.lat,
+            self.drone.home_location.lon,
+            self.drone.home_location.alt,
+        )
+        if not result:
+            return False, None
+
+        return True, location
---
+++ mavlink/test_flight_controller.py
@@ -0,0 +1,45 @@
+"""
+Test for flight input device by printing to console.
+"""
+import sys
+import time
+
+from modules import flight_controller
+
+
+DELAY_TIME = 0.5  # seconds
+MISSION_PLANNER_ADDRESS = "tcp:127.0.0.1:14550"
+TIMEOUT = 1.0  # seconds
+
+
+if __name__ == "__main__":
+    result, controller = flight_controller.FlightController.create(MISSION_PLANNER_ADDRESS)
+    if not result:
+        print("failed")
+        sys.exit()
+
+    # Get Pylance to stop complaining
+    assert controller is not None
+
+    for i in range(5):
+        result, odometry = controller.get_odometry()
+        if result:
+            print("lat: " + str(odometry.position.latitude))
+            print("lon: " + str(odometry.position.longitude))
+            print("alt: " + str(odometry.position.altitude))
+            print("yaw: " + str(odometry.orientation.yaw))
+            print("roll: " + str(odometry.orientation.roll))
+            print("pitch: " + str(odometry.orientation.pitch))
+            print("")
+        else:
+            print("failed")
+
+        result, home = controller.get_home_location(TIMEOUT)
+        if result:
+            print("lat: " + str(home.latitude))
+            print("lon: " + str(home.longitude))
+            print("alt: " + str(home.altitude))
+
+        time.sleep(DELAY_TIME)
+
+    print("Done!")
