553152c2000cecb3574e54f96ff6610091a43945
Xierumeng <xierumeng@hotmail.com>
2023-11-07 19:04:31 -0800

    KML file generation avoids name collision (#18)

--- kml/modules/waypoints_to_kml.py
+++ kml/modules/waypoints_to_kml.py
@@ -2,13 +2,14 @@
 Module to convert waypoints list to kml document.
 """
 import pathlib
+import time
 
 import simplekml
 
 
 def waypoints_to_kml(waypoints: "list[tuple[float, float]]",
-                     document_name: str,
-                     save_directory: pathlib.Path) -> bool:
+                     document_name_prefix: str,
+                     save_directory: pathlib.Path) -> "tuple[bool, pathlib.Path | None]":
     """
     Generates KML file from a list of waypoints.
 
@@ -16,8 +17,8 @@ def waypoints_to_kml(waypoints: "list[tuple[float, float]]",
     ----------
     waypoints: list[tuple[float, float]]
         Waypoint coordinates in decimal degrees (latitude, longitude).
-    document_name: str
-        Name of the KML file to save (without the .kml extension).
+    document_name_prefix: str
+        Name of the KML file to save (without the timestamp or .kml extension).
     save_directory: pathlib.Path
         Path to save the KML file to.
 
@@ -35,11 +36,13 @@ def waypoints_to_kml(waypoints: "list[tuple[float, float]]",
         # coords parameters are in the order: lon, lat, optional height
         kml.newpoint(name=waypoint_name, coords=[(lng, lat)])
 
-    kml_file_path = pathlib.Path(save_directory, f"{document_name}.kml")
+    kml_file_path = pathlib.Path(save_directory, f"{document_name_prefix}_{int(time.time())}.kml")
 
     try:
         kml.save(str(kml_file_path))
-        return True
+        return True, kml_file_path
+    # Required for catching library exceptions
+    # pylint: disable-next=broad-exception-caught
     except Exception as e:
         print(f"Error while saving KML file: {e}")
-        return False
+        return False, None
