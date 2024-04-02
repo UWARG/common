788a2b5b78f09c8b89776cffd00e0fab03c1f583
Maxwell Lou <maxlou2005@gmail.com>
2024-04-02 10:50:12 -0400

    moved image_decode.py into module folder. Added test file template. TODO: Finish writing tests when image encode is finished

---
+++ lte/test_image_encode_decode.py
@@ -0,0 +1,23 @@
+"""
+Test image encode and decode.
+TODO: finish image encode and then write this test
+"""
+from lte.modules import image_decode
+
+def main() -> int:
+    # TODO: Get test images and Encode
+
+    # Decode
+    img_array = image_decode.decode()
+
+    # Do checks
+    # eg. check output shape
+    return 0
+
+if __name__ == "__main__":
+    result_main = main()
+
+    if result_main < 0:
+        print(f"ERROR: Status code: {result_main}")
+
+    print("Done!")
