083b67c507283edbaa2218c332b9d44977cbeba3
Maxwell Lou <62454180+maxlou05@users.noreply.github.com>
2024-04-05 21:46:30 -0400

    decode JPEG image data to numpy array (#29)
    * decode JPEG image data to numpy array
    
    * moved image_decode.py into module folder. Added test file template. TODO: Finish writing tests when image encode is finished
    
    * added dummy variable
    
    * linter changes

---
+++ lte/modules/image_decode.py
@@ -0,0 +1,24 @@
+"""
+Decodes images from JPEG bytes to numpy array.
+"""
+
+import io
+
+from PIL import Image
+import numpy as np
+
+
+def decode(data: "io.BytesIO | bytes") -> np.ndarray:
+    """
+    Decodes a JPEG encoded image and returns it as a numpy array.
+
+    Args:
+        data: bytes object containing the JPEG encoded image
+        TODO: When encoding JPEG, make sure the input image only has 3 channels,
+              if original picture was a PNG (filter out A channel)
+    Returns:
+        NDArray with in RGB format. Shape is (Height, Width, 3)
+    """
+    image = Image.open(data, formats=["JPEG"])
+
+    return np.asarray(image)
