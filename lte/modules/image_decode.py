8928bab636da1f9ed76108ed1b6ed3848f7aa658
isabelle huang <88785156+isabellehuangg@users.noreply.github.com>
2024-06-03 22:33:22 -0400

    encode image data to JPEG (#30)
    * image encoding
    
    * change parameter to numpy array
    
    * modify comment
    
    * update image encode and complete integration test for encode and decode
    
    * Fix linter
    
    ---------
    
    Co-authored-by: Maxwell Lou <maxlou2005@gmail.com>

--- lte/modules/image_decode.py
+++ lte/modules/image_decode.py
@@ -14,8 +14,6 @@ def decode(data: "io.BytesIO | bytes") -> np.ndarray:
 
     Args:
         data: bytes object containing the JPEG encoded image
-        TODO: When encoding JPEG, make sure the input image only has 3 channels,
-              if original picture was a PNG (filter out A channel)
     Returns:
         NDArray with in RGB format. Shape is (Height, Width, 3)
     """
