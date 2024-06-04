ce1ef66c780e3a0510a5a7a818edc38cf0ff278c
Maxwell Lou <maxlou2005@gmail.com>
2024-06-03 20:51:04 -0400

    update image encode and complete integration test for encode and decode

--- lte/modules/image_encode.py
+++ lte/modules/image_encode.py
@@ -7,9 +7,22 @@ import io
 from PIL import Image
 import numpy as np
 
+
+QUALITY = 80  # Quality of JPEG encoding to use (0-100)
+
+
 def encode(image_array: np.ndarray) -> "io.BytesIO | bytes":
-    img = Image.fromarray(image_array)
+    """
+    Encodes an image in numpy array form into bytes of a JPEG.
+
+    Args:
+        image_array: numpy array of an RGB image with shape (Height, Width, 3)
+    Returns:
+        bytes which form the JPEG encoded image
+    """
+    img = Image.fromarray(image_array, mode="RGB")
 
     buffer = io.BytesIO()
-    img.save(buffer, format = 'JPEG', quality=100)
-    return buffer.getvalue()
+    img.save(buffer, format="JPEG", quality=QUALITY)
+
+    return buffer
--- lte/test_image_encode_decode.py
+++ lte/test_image_encode_decode.py
@@ -1,20 +1,37 @@
 """
 Test image encode and decode.
-TODO: finish image encode and then write this test
 """
 
+import pathlib
+
+from PIL import Image
+import numpy as np
+
 from lte.modules import image_decode
+from lte.modules import image_encode
 
 
 def main() -> int:
-    # TODO: Get test images and Encode
-    raw_data = 0
+    # Get test image in numpy form
+    im = Image.open(pathlib.Path("lte", "test.png"))
+    raw_data = np.asarray(im)
 
-    # Decode
-    img_array = image_decode.decode(raw_data)
+    # Encode image into JPEG
+    jpeg_bytes = image_encode.encode(raw_data)
 
-    # Do checks
-    # eg. check output shape
+    # Decode JPEG image back to numpy
+    img_array = image_decode.decode(jpeg_bytes)
+
+    # Reconstruct the image (for human viewing/comparison)
+    result = Image.fromarray(img_array, mode="RGB")
+    result.save(pathlib.Path("lte", "result.jpg"), quality=80)
+
+    # Check output shape
+    assert img_array.shape == raw_data.shape
+
+    # Note: the following fail since JPEG encoding is lossy
+    # assert (raw_data == img_array).all()
+
     return 0
 
 
