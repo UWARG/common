77715f4d90f208045127feb8f27cde252a1cc69c
Jonathan Yuan <jy90023@gmail.com>
2024-06-08 18:17:26 -0400

    Refactor pytest into sender and receiver scripts

--- network/README.md
+++ network/README.md
@@ -1,4 +1,5 @@
 # Network
+<<<<<<< HEAD
 This module facilitates communication over TCP and UDP.
 
 ## Testing
@@ -29,3 +30,16 @@ This process repeats until all test messages are sent.
 
 *Note: UDP does not guarantee that data is sent or is not corrupted.
 It is a connectionless protocol and thus the server cannot send any messages.
+=======
+To test `client_socket` and `server_socket`, we will set up a socket server that listens for image byte data from a sender.
+
+Run the test scripts in the following order:
+
+1. `test_receiver.py`
+2. `test_sender.py`
+
+`test_receiver.py` will start a socket server listening on `127.0.0.1:8080`. `test_sender.py` will first encode each image returned from `get_images`, then send the raw byte data to the server. 
+
+The main loop expects a response from the server, consisting of the original image, data before continuing to the next image. The asserted condition in the main loop tests for equality between the image byte data that was sent to the server, and the byte data that was received, ensuring data integrity.
+
+>>>>>>> 8c58ed6 (Refactor pytest into sender and receiver scripts)
---
+++ network/test_sender.py
@@ -0,0 +1,100 @@
+"""
+Test socket operations by sending images over client sockets.
+"""
+
+import struct
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from network.modules.server_socket import ClientSocket
+
+
+IMAGE_PATH = Path(__file__).resolve().parent
+SOCKET_ADDRESS = "127.0.0.1"
+SOCKET_PORT = 8080
+
+
+IMAGE_ENCODE_EXT = ".png"
+
+
+def get_images() -> "list[bytes]":
+    """
+    Returns a list of images in byte representation.
+    """
+    return [
+        image_encode(cv2.imread(str(IMAGE_PATH / "test_images/landing_pad_1.png")))[1],
+        image_encode(cv2.imread(str(IMAGE_PATH / "test_images/landing_pad_2.png")))[1],
+    ]
+
+
+def image_encode(image: "np.ndarray") -> "tuple[bool, bytes | None]":
+    """
+    Encodes an image (np.ndarray as an RGB matrix) and returns its byte sequence.
+    """
+    result, encoded_image = cv2.imencode(IMAGE_ENCODE_EXT, image)
+    if not result:
+        return False, None
+
+    encoded_image_bytes = encoded_image.tobytes()
+
+    return True, encoded_image_bytes
+
+
+def recv_all(client_socket: ClientSocket, data_len: int) -> tuple[bool, bytes | None]:
+    """
+    Receives an image of data_len bytes from a socket
+    """
+    image_data = b""
+    data_recv = 0
+    while data_recv < data_len:
+        result, data = client_socket.recv(data_len - data_recv)
+        if not result:
+            return False, None
+
+        print(f"Received {len(data)} bytes.")
+        data_recv += len(data)
+        image_data += data
+
+    return True, image_data
+
+
+def start_sender() -> int:
+    """
+    Client will send landing pad images to the server, and the server will send them back.
+    """
+    result, client_socket = ClientSocket.create(  # pylint: disable=unpacking-non-sequence
+        host=SOCKET_ADDRESS, port=SOCKET_PORT
+    )
+    assert result, "Failed to create ClientSocket."
+    print(f"Connected to: {SOCKET_ADDRESS}:{SOCKET_PORT}.")
+
+    for image in get_images:
+        # Send image byte length, 4 byte message
+        data_len = struct.pack("<I", len(image))
+        result = client_socket.send(data_len)
+        assert result, "Failed to send image byte length."
+        print("Sent image byte length to server.")
+
+        result = client_socket.send(image)
+        assert result, "Failed to send image data."
+        print("Sent image data to server.")
+
+        image_data = recv_all(client_socket, len(image))
+        print("Received image data from server.")
+        assert image == image_data, "Sent image bytes does not match received image bytes"
+
+    result = client_socket.close()
+    assert result, "Failed to close client connection."
+
+    print("Connection to server closed.")
+
+
+
+if __name__ == "__main__":
+    result = start_sender(SOCKET_ADDRESS, SOCKET_PORT)
+    if result < 0:
+        print(f"ERROR: Status code: {result}")
+
+    print("Done!")
