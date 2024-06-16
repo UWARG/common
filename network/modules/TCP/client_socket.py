781edf0d34f7d4e134384caf645e1405455ece6d
Maxwell Lou <maxlou2005@gmail.com>
2024-06-16 17:35:35 -0400

    - Refactored to indicate current sockets are TCP specific - Simplified many steps by using higher-level APIs - Added some comments and changed README

---
+++ network/modules/TCP/client_socket.py
@@ -0,0 +1,80 @@
+"""
+Wrapper for client socket operations.
+"""
+
+import socket
+
+from network.modules.TCP.socket_wrapper import TcpSocket
+
+
+class TcpClientSocket(TcpSocket):
+    """
+    Wrapper for client socket operations.
+    """
+
+    __create_key = object()
+
+    def __init__(self, class_private_create_key: object, socket_instance: socket.socket) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is TcpClientSocket.__create_key, "Use create() method"
+
+        super().__init__(socket_instance=socket_instance)
+
+    @classmethod
+    def create(
+        cls,
+        instance: socket.socket = None,
+        host: str = "localhost",
+        port: int = 5000,
+        connect_timeout: float = 10.0,
+    ) -> "tuple[bool, TcpClientSocket | None]":
+        """
+        Establishes socket connection through provided host and port.
+
+        Parameters
+        ----------
+        instance: socket.socket (default None)
+            For initializing Socket with an existing socket object.
+
+        host: str (default "localhost")
+        port: int (default 5000)
+            The host combined with the port will form an address (e.g. localhost:5000)
+
+        connect_timeout: float (default 10.0)
+            Timeout for establishing connection, in seconds
+
+        Returns
+        -------
+        tuple[bool, TcpClientSocket | None]
+            The first parameter represents if the socket creation is successful.
+            - If it is not successful, the second parameter will be None.
+            - If it is successful, the second parameter will be the created
+                TcpClientSocket object.
+        """
+        # Reassign instance before check or Pylance will complain
+        socket_instance = instance
+        if socket_instance is not None:
+            return True, TcpClientSocket(cls.__create_key, socket_instance)
+
+        if connect_timeout <= 0:
+            # raise valueError?
+            # Zero puts it on non-blocking mode, which complicates things
+            print("Must be a positive non-zero value")
+            return False, None
+
+        try:
+            socket_instance = socket.create_connection((host, port), connect_timeout)
+            return True, TcpClientSocket(cls.__create_key, socket_instance)
+        except TimeoutError as e:
+            print("Connection timed out.")
+        except socket.gaierror as e:
+            print(
+                f"Could not connect to socket, address related error: {e}. "
+                "Make sure the host and port are correct."
+            )
+        except socket.error as e:
+            print(f"Could not connect to socket, connection error: {e}.")
+
+        return False, None
---
+++ network/modules/TCP/server_socket.py
@@ -0,0 +1,108 @@
+"""
+Wrapper for server socket operations.
+"""
+
+import socket
+
+from network.modules.TCP.socket_wrapper import TcpSocket
+
+
+class TcpServerSocket(TcpSocket):
+    """
+    Wrapper for server socket operations.
+    """
+
+    __create_key = object()
+
+    def __init__(self, class_private_create_key: object, socket_instance: socket.socket) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is TcpServerSocket.__create_key, "Use create() method"
+
+        super().__init__(socket_instance=socket_instance)
+
+    @classmethod
+    def create(
+        cls,
+        instance: socket.socket = None,
+        host: str = "",
+        port: int = 5000,
+    ) -> "tuple[bool, TcpServerSocket | None]":
+        """
+        Establishes socket connection through provided host and port.
+            Note: Although in practice a TCP 'server' simply connects 2 clients together,
+            this newly created client is called 'server' for simplicity and differentiation.
+
+        Parameters
+        ----------
+        instance: socket.socket (default None)
+            For initializing Socket with an existing socket object.
+
+        host: str (default "")
+            Empty string is interpreted as '0.0.0.0' (IPv4) or '::' (IPv6), which is all addresses.
+            Could also use socket.gethostname(). (needed to enable other machines to connect)
+        port: int (default 5000)
+            The host combined with the port will form an address (e.g. localhost:5000)
+
+        Returns
+        -------
+        tuple[bool, TcpServerSocket | None]
+            The first parameter represents if the socket creation is successful.
+            - If it is not successful, the second parameter will be None.
+            - If it is successful, the second parameter will be the created
+              ServerSocket object.
+        """
+        # Reassign instance before check or Pylance will complain
+        socket_instance = instance
+        if socket_instance is not None:
+            return True, TcpServerSocket(cls.__create_key, socket_instance)
+
+        if socket.has_dualstack_ipv6():
+            # Create server which can accept both IPv6 and IPv4 if possible
+            try:
+                server = socket.create_server(
+                    (host, port),
+                    family=socket.AF_INET6,
+                    dualstack_ipv6=True,
+                )
+            except socket.gaierror as e:
+                print(
+                    f"Could not connect to socket, address related error: {e}. "
+                    "Make sure the host and port are correct."
+                )
+                return False, None
+            except socket.error as e:
+                print(f"Could not connect to socket, connection error: {e}.")
+                return False, None
+        else:
+            # Otherwise, server can only accept IPv4
+            try:
+                server = socket.create_server((host, port))
+            except socket.gaierror as e:
+                print(
+                    f"Could not connect to socket, address related error: {e}. "
+                    "Make sure the host and port are correct."
+                )
+                return False, None
+            except socket.error as e:
+                print(f"Could not connect to socket, connection error: {e}.")
+                return False, None
+
+        # Currently listening, waiting for a connection
+        if host == "":
+            print(f"Listening for external connections on port {port}")
+        else:
+            print(f"Listening for internal connections on {host}:{port}")
+
+        # Since this is in blocking mode, nothing can happen here, including keyboard interrupt
+
+        socket_instance, addr = server.accept()
+
+        # Now, a connection as been accepted (created a new 'client' socket)
+        print(f"Accepted a connection from {addr[0]}:{addr[1]}")
+
+        server.close()
+        print("No longer accepting new connections.")
+
+        return True, TcpServerSocket(cls.__create_key, socket_instance)
