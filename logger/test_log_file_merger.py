8093ef13bf4740f648c6b8ba6aeeb74fbc8da20e
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-10-14 22:07:22 -0400

    Created tests for new functions

--- logger/test_log_file_merger.py
+++ logger/test_log_file_merger.py
@@ -2,48 +2,94 @@
 Log file merger unit tests.
 """
 
-import os
-import tempfile
+import pathlib
 import shutil
 import pytest
-
 
 from .modules import log_file_merger
 
 
+TEMP_LOGS_DIRECTORY = pathlib.Path("logs_temp")
+MERGED_LOGS_FILENAME = pathlib.Path("merged_logs")
+UNSORTED_LOG_ENTRIES = [
+    "12:59:28: [INFO] [foo1.py | foo1 | 43] Foo1 initialized\n",
+    "13:00:04: [ERROR] [foo1.py | foo1 | 30] Foo1 could not be created\n",
+    "13:00:22: [ERROR] [foo1.py | foo1 | 49] Foo1 failed to create class object\n",
+    "12:59:40: [INFO] [foo2.py | foo2 | 43] Foo2 initialized\n",
+    "13:00:06: [ERROR] [foo2.py | foo2 | 30] Foo2 could not be created\n",
+    "13:00:09: [ERROR] [foo2.py | foo2 | 49] Foo2 failed to create class object\n",
+    "12:59:59: [INFO] [foo3.py | foo3 | 43] Foo3 initialized\n",
+    "13:00:12: [ERROR] [foo3.py | foo3 | 30] Foo3 could not be created\n",
+    "13:00:30: [ERROR] [foo3.py | foo3 | 49] Foo3 failed to create class object\n",
+]
+SORTED_LOG_ENTRIES = [
+    "12:59:28: [INFO] [foo1.py | foo1 | 43] Foo1 initialized\n",
+    "12:59:40: [INFO] [foo2.py | foo2 | 43] Foo2 initialized\n",
+    "12:59:59: [INFO] [foo3.py | foo3 | 43] Foo3 initialized\n",
+    "13:00:04: [ERROR] [foo1.py | foo1 | 30] Foo1 could not be created\n",
+    "13:00:06: [ERROR] [foo2.py | foo2 | 30] Foo2 could not be created\n",
+    "13:00:09: [ERROR] [foo2.py | foo2 | 49] Foo2 failed to create class object\n",
+    "13:00:12: [ERROR] [foo3.py | foo3 | 30] Foo3 could not be created\n",
+    "13:00:22: [ERROR] [foo1.py | foo1 | 49] Foo1 failed to create class object\n",
+    "13:00:30: [ERROR] [foo3.py | foo3 | 49] Foo3 failed to create class object\n",
+]
+
+
+@pytest.fixture(name="temp_path")
+def fixture_temp_path() -> pathlib.Path:  # type: ignore
+    """
+    Returns the path to a temporary log directory with dummy log subdirectories.
+    """
+    # Create a temporary logs directory
+    temp_logs_path = TEMP_LOGS_DIRECTORY
+    temp_logs_path.mkdir(parents=True, exist_ok=True)
+
+    # Create sample directories with timestamped names
+    (temp_logs_path / "2024-10-10_10-00-00").mkdir()
+    (temp_logs_path / "2024-10-11_10-00-00").mkdir()
+    (temp_logs_path / "2024-10-09_10-00-00").mkdir()
+
+    yield temp_logs_path
+
+    # Remove the temporary logs directory
+    shutil.rmtree(temp_logs_path)
+
+
 @pytest.fixture(name="dummy_logs")
-def fixture_dummy_logs() -> str:  # type: ignore
+def fixture_dummy_logs() -> pathlib.Path:  # type: ignore
     """
     Returns the path to a temporary directory with dummy log files.
     """
-    temp_directory = tempfile.mkdtemp()
-    subdirectory = os.path.join(temp_directory, "subdirectory")
-    os.makedirs(subdirectory)
+    subdirectory = TEMP_LOGS_DIRECTORY / "subdirectory"
+    subdirectory.mkdir(parents=True, exist_ok=True)
 
     # Create three dummy log files in the subdirectory
-    log_file_1 = os.path.join(subdirectory, "log1.log")
-    log_file_2 = os.path.join(subdirectory, "log2.log")
-    log_file_3 = os.path.join(subdirectory, "log3.log")
+    log_file_1 = subdirectory / "log1.log"
+    log_file_2 = subdirectory / "log2.log"
+    log_file_3 = subdirectory / "log3.log"
 
-    with open(log_file_1, "w", encoding="utf-8") as f:
-        f.write("12:59:28: [INFO] [foo1.py | foo1 | 43] Foo1 initialized\n")
-        f.write("13:00:04: [ERROR] [foo1.py | foo1 | 30] Foo1 could not be created\n")
-        f.write("13:00:22: [ERROR] [foo1.py | foo1 | 49] Foo1 failed to create class object\n")
+    log_file_1.write_text(
+        "12:59:28: [INFO] [foo1.py | foo1 | 43] Foo1 initialized\n"
+        "13:00:04: [ERROR] [foo1.py | foo1 | 30] Foo1 could not be created\n"
+        "13:00:22: [ERROR] [foo1.py | foo1 | 49] Foo1 failed to create class object\n",
+        encoding="utf-8",
+    )
 
-    with open(log_file_2, "w", encoding="utf-8") as f:
-        f.write("12:59:40: [INFO] [foo2.py | foo2 | 43] Foo2 initialized\n")
-        f.write("13:00:06: [ERROR] [foo2.py | foo2 | 30] Foo2 could not be created\n")
-        f.write("13:00:09: [ERROR] [foo2.py | foo2 | 49] Foo2 failed to create class object\n")
+    log_file_2.write_text(
+        "12:59:40: [INFO] [foo2.py | foo2 | 43] Foo2 initialized\n"
+        "13:00:06: [ERROR] [foo2.py | foo2 | 30] Foo2 could not be created\n"
+        "13:00:09: [ERROR] [foo2.py | foo2 | 49] Foo2 failed to create class object\n",
+        encoding="utf-8",
+    )
 
-    with open(log_file_3, "w", encoding="utf-8") as f:
-        f.write("12:59:59: [INFO] [foo3.py | foo3 | 43] Foo3 initialized\n")
-        f.write("13:00:12: [ERROR] [foo3.py | foo3 | 30] Foo3 could not be created\n")
-        f.write("13:00:30: [ERROR] [foo3.py | foo3 | 49] Foo3 failed to create class object\n")
+    log_file_3.write_text(
+        "12:59:59: [INFO] [foo3.py | foo3 | 43] Foo3 initialized\n"
+        "13:00:12: [ERROR] [foo3.py | foo3 | 30] Foo3 could not be created\n"
+        "13:00:30: [ERROR] [foo3.py | foo3 | 49] Foo3 failed to create class object\n",
+        encoding="utf-8",
+    )
 
-    yield temp_directory
-
-    # Cleanup
-    shutil.rmtree(temp_directory)
+    return subdirectory
 
 
 class TestLogFileMerger:
@@ -51,34 +97,64 @@ class TestLogFileMerger:
     Test suite for the log file merger.
     """
 
-    def test_merge_log_files(self, dummy_logs: str) -> None:
+    def test_get_current_log_directory(self, temp_path: pathlib.Path) -> None:
         """
-        Test if merger correctly combines log files.
+        Test successful retrieval of the most recent run directory.
         """
-        temp_directory = dummy_logs
-        subdirectory = os.path.join(temp_directory, "subdirectory")
+        # Use the fixture to get the temporary directory with the sample structure
+        temp_logs_path = temp_path
 
-        # Merge log files in the subdirectory
-        log_file_merger.merge_log_files(subdirectory)
+        # Define the datetime format used in the folder names
+        file_datetime_format = "%Y-%m-%d_%H-%M-%S"
 
-        # Check if merged_logs.log is created
-        merged_log_file = os.path.join(subdirectory, "merged_logs.log")
-        assert os.path.exists(merged_log_file)
+        # Call the function and get the result
+        success, current_run_directory = log_file_merger.get_current_run_directory(
+            temp_logs_path, file_datetime_format
+        )
 
-        # Check the contents of the merged log file
-        with open(merged_log_file, "r", encoding="utf-8") as f:
-            merged_logs = f.readlines()
+        # Assertions
+        assert success
+        assert current_run_directory == temp_logs_path / "2024-10-11_10-00-00"
 
-        expected_logs = [
-            "12:59:28: [INFO] [foo1.py | foo1 | 43] Foo1 initialized\n",
-            "12:59:40: [INFO] [foo2.py | foo2 | 43] Foo2 initialized\n",
-            "12:59:59: [INFO] [foo3.py | foo3 | 43] Foo3 initialized\n",
-            "13:00:04: [ERROR] [foo1.py | foo1 | 30] Foo1 could not be created\n",
-            "13:00:06: [ERROR] [foo2.py | foo2 | 30] Foo2 could not be created\n",
-            "13:00:09: [ERROR] [foo2.py | foo2 | 49] Foo2 failed to create class object\n",
-            "13:00:12: [ERROR] [foo3.py | foo3 | 30] Foo3 could not be created\n",
-            "13:00:22: [ERROR] [foo1.py | foo1 | 49] Foo1 failed to create class object\n",
-            "13:00:30: [ERROR] [foo3.py | foo3 | 49] Foo3 failed to create class object\n",
-        ]
+    def test_read_log_files(self, dummy_logs: "list[str]") -> None:
+        """
+        Test correct reading of log files.
+        """
+        # Read log files in subdirectory and get the combined, unsorted list of log entries
+        result, log_entries = log_file_merger.read_log_files(dummy_logs)
 
-        assert merged_logs == expected_logs
+        assert result
+        assert log_entries == UNSORTED_LOG_ENTRIES
+
+    def test_sort_log_files(self) -> None:
+        """
+        Test correct sorting of log files.
+        """
+        # Read log files in subdirectory and get the combined, unsorted list of log entries
+        result, sorted_log_entries = log_file_merger.sort_log_entries(UNSORTED_LOG_ENTRIES)
+
+        assert result
+        assert sorted_log_entries == SORTED_LOG_ENTRIES
+
+    def test_write_merged_logs(self, temp_path: pathlib.Path) -> None:
+        """
+        Test writing the sorted log entries into the merged log file.
+        """
+        temp_logs_path = temp_path
+
+        # Write the sorted log entries to the output file
+        result, merged_log_file = log_file_merger.write_merged_logs(
+            SORTED_LOG_ENTRIES, temp_logs_path
+        )
+
+        # Check if the function returned True (successful write)
+        assert result
+
+        # Verify the contents of the output file
+        written_contents = merged_log_file.read_text(encoding="utf-8")
+
+        # Create an expected output string
+        expected_output = "".join(SORTED_LOG_ENTRIES)
+
+        # Compare the written content with the expected output
+        assert written_contents == expected_output
