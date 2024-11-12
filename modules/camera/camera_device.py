740eeae21439318bd9d339a1302ba56e48048e29
Alex <alex.yimo.liu@gmail.com>
2024-11-12 13:04:40 -0500

    Update camera_device.py
    There may be image format issues, not sure how to test impl

--- modules/camera/camera_device.py
+++ modules/camera/camera_device.py
@@ -8,24 +8,33 @@ import time
 import cv2
 import numpy as np
 
+import picamera2
+
 
 class CameraDevice:
     """
     Wrapper for camera.
     """
 
-    def __init__(self, name: "int | str", save_nth_image: int = 0, save_name: str = "") -> None:
+    def __init__(
+        self, name: "int | str", save_nth_image: int = 0, save_name: str = "", use_pc2: bool = False
+    ) -> None:
         """
         name: Device name or index (e.g. /dev/video0 ).
         (optional) save_nth_image: For debugging, saves every nth image.
             A value of 0 indicates no images should be saved
         (optional) save_name: For debugging, file name for saved images.
+        (optional) use_pc2: Use picamera2 implementation instead of opencv impl
         """
-        self.__camera = cv2.VideoCapture(name)
-        assert self.__camera.isOpened()
-
-        self.__camera.set(cv2.CAP_PROP_FRAME_WIDTH, sys.maxsize)
-        self.__camera.set(cv2.CAP_PROP_FRAME_HEIGHT, sys.maxsize)
+        self.__using_pc2 = use_pc2
+        if self.__using_pc2:
+            self.__camera = picamera2.Picamera2()
+            self.__camera.start(show_preview=False)
+        else:
+            self.__camera = cv2.VideoCapture(name)
+            assert self.__camera.isOpened()
+            self.__camera.set(cv2.CAP_PROP_FRAME_WIDTH, sys.maxsize)
+            self.__camera.set(cv2.CAP_PROP_FRAME_HEIGHT, sys.maxsize)
 
         self.__divisor = save_nth_image
         self.__counter = 0
@@ -37,13 +46,15 @@ class CameraDevice:
         """
         Destructor.
         """
-        self.__camera.release()
+        if not self.__using_pc2:
+            self.__camera.release()
 
     def get_image(self) -> "tuple[bool, np.ndarray]":
         """
         Take a picture with the camera
         """
-        result, image = self.__camera.read()
+
+        result, image = self.__camera.capture_array() if self.__using_pc2 else self.__camera.read()
         if not result:
             return False, None
 
