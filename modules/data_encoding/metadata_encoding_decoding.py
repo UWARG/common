56900b71b1bcf138cf394de10c83295b966ddc5a
Daniel Suzuki <53331046+1Two3Four@users.noreply.github.com>
2024-11-30 19:30:59 -0500

    Encoding and Decoding Global Positions (Task: Message encoding/decoding (in common)) (#66)
    * Created encode and decode global position function and created unit test for functions. Created new directory called data_encoding and data_encoding_tests.
    
    * changed encoding_decoding function to have one byte at the start dedicated to class ID of the worker which called the function; modified unit test. Added worker_enum class.
    
    * reformatted
    
    * moved imports around
    
    * got pytest working
    
    * removed .DS_Store hopefully
    
    * testing
    
    * deleting .DS_Store
    
    * changed message_encoding so it returns the worker class instead of worker ID when decoding. Added metadata_en
    coding function and corresponding test file.
    
    * forgot to change docstring for test_encoding_decoding_metadata. Fixed it. Changed WorkerEnum class to not include example Workers
    
    * removed all print statements, changed import syntax to match organization standards
    
    * Changed import to only import from modules. Created and modified __init__.py folder for modules/data_encoding.
    
    * emptied _init_. Made syntax adhere to organization standards. Streamlined test functions
    
    * alphabetical order for imports
    
    * fixed syntax and spacing.
    
    * Some of previous files didn't get pushed.
    
    * changed name of test_encoding_decoding to test_message_encoding_decoding.
    
    * Changed logic in tests to make it work.
    
    * Fixed Logic of Tests again
    
    * removed space in docstring of worker_enum.py
    
    ---------
    
    Co-authored-by: Daniel Suzuki

---
+++ modules/data_encoding/metadata_encoding_decoding.py
@@ -0,0 +1,70 @@
+"""
+Encoding and Decoding Metadata
+Save first byte as char to represent which worker sent the message 
+"""
+
+import struct
+
+from . import worker_enum
+
+
+def encode_metadata(
+    worker_name: str, number_of_messages: int
+) -> "tuple[True, bytes] | tuple[False, None]":
+    """
+    Encode PositionGlobal object into Bytes. Worker_ID to be encoded as the first byte of the message
+
+    Parameters:
+       worker_name: name of the worker defined by the name of its own file (worker_name = pathlib.Path(__file__).stem)
+       number_of_messages: number of messages intended to be sent
+
+    Returns:
+        packed_coordinates (bytes): Encoded int corresponding to number of messages as bytes.
+        First byte dependant on which worker is calling the funciton, value depends on its corresponding enum value (see worker_enum.py)
+    """
+    try:
+        worker_id = worker_enum.WorkerEnum[worker_name.upper()]
+        if not worker_id:  # If worker ID is not in the Enum Class
+            return False, None
+
+        # Encode message using PositionGlobal's latitude, longitude, altitude, with the worker ID in the front
+        packed_metadata = struct.pack(
+            "=Bi",
+            worker_id.value,  # 1 byte (char)
+            number_of_messages,  # 4 bytes
+            # 5 bytes total
+        )
+    except (struct.error, AttributeError, ValueError):
+        return False, None
+
+    return True, packed_metadata
+
+
+def decode_metadata(
+    encoded_metadata: bytes,
+) -> "tuple[True, worker_enum.WorkerEnum, int] | tuple[False, None, None]":
+    """
+    Decode bytes into a PositionGlobal object.
+
+    Parameters:
+        encoded_message (bytes): Encoded bytearray containing Worker message ID, number of messages sent
+
+    Returns:
+        Tuple: success, WorkerEnum member instance corresponding to ID, number of messages received as an integer
+    """
+    # Unpack the byte sequence
+    try:
+        if len(encoded_metadata) != struct.calcsize(
+            "=Bi"
+        ):  # should equal 5 bytes: 1 unsigned char + 1 int
+            return False, None, None
+
+        worker_id = struct.unpack("B", encoded_metadata[:1])[
+            0
+        ]  # unpack returns tuple (unsigned char,) so [0] is needed
+        number_of_messages = struct.unpack("i", encoded_metadata[1:])[0]
+    except struct.error:
+        return False, None, None
+
+    # Create and return a PositionGlobal object
+    return True, worker_enum.WorkerEnum(worker_id), number_of_messages
---
+++ modules/data_encoding/worker_enum.py
@@ -0,0 +1,28 @@
+"""
+Enumeration for Worker Types.
+
+This enum class defines unique identifiers for different worker types in the system.
+Each worker type is associated with a specific task, represented by a unique integer ID.
+
+Workers can be found in UWARG/computer-vision-python Github repository
+
+Usage:
+    This enum can be used to identify and categorize workers programmatically.
+    For example, `Worker_Enum.COMMUNICATIONS_WORKER.value` will return the integer ID `3`.
+"""
+
+from enum import Enum
+
+
+class WorkerEnum(Enum):
+    """
+    Enum class for worker classes. Acts as message ID
+    """
+
+    CLUSTER_ESTIMATION_WORKER = 1
+    COMMUNICATIONS_WORKER = 2
+    DATA_MERGE_WORKER = 3
+    DETECT_TARGET_WORKER = 4
+    FLIGHT_INTERFERENCE_WORKER = 5
+    GEOLOCATION_WORKER = 6
+    VIDEO_INPUT_WORKER = 7
