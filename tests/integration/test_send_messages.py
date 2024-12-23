452231338c9a0a511be85cbf9636d7a3bbc88ef4
lilyjge <lilyjge@gmail.com>
2024-12-23 12:56:09 -0500

    Main guarded test_send_messages

--- tests/integration/test_send_messages.py
+++ tests/integration/test_send_messages.py
@@ -3,32 +3,45 @@ Test script to send messages to the drone using the FlightController class.
 """
 
 import time
-import sys
 
 from pymavlink import mavutil
 
 from modules.mavlink.flight_controller import FlightController
 
-# Connect to the vehicle
-success, controller = FlightController.create("tcp:localhost:5672")
-if not success:
-    print("Failed to connect")
-    sys.exit(1)
+
+def main() -> int:
+    """
+    Main function.
+    """
+    # Connect to the vehicle
+    success, controller = FlightController.create("tcp:localhost:5672")
+    if not success:
+        print("Failed to connect")
+        return 1
+
+    messages = [  # 10 random messages
+        "System startup",
+        "Initializing sensors",
+        "GPS lock acquired",
+        "Motors armed",
+        "Taking off",
+        "Reaching altitude",
+        "Mission started",
+        "Waypoint reached",
+        "Returning home",
+        "Landing sequence initiated",
+    ]
+
+    for msg in messages:
+        controller.send_statustext_msg(msg, mavutil.mavlink.MAV_SEVERITY_INFO)
+        time.sleep(1)  # Wait 1 second between messages
+
+    return 0
 
 
-messages = [  # 10 random messages
-    "System startup",
-    "Initializing sensors",
-    "GPS lock acquired",
-    "Motors armed",
-    "Taking off",
-    "Reaching altitude",
-    "Mission started",
-    "Waypoint reached",
-    "Returning home",
-    "Landing sequence initiated",
-]
+if __name__ == "__main__":
+    result_main = main()
+    if result_main != 0:
+        print(f"ERROR: Status code: {result_main}")
 
-for msg in messages:
-    controller.send_statustext_msg(msg, mavutil.mavlink.MAV_SEVERITY_INFO)
-    time.sleep(1)  # Wait 1 second between messages
+    print("Done!")
