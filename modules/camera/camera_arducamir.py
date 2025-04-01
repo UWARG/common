e8fcaa55b07e443a32063c161942801b1543b20a
Oliad Rundassa <68521080+Oliad-R@users.noreply.github.com>
2025-04-01 11:39:52 -0400

    initial creation of ArducamIR class, output enum, & helper functions (#81)
    * initial creation of ArducamIR class, output enum, & helper functions
    
    * fixed pyproject.toml to ignore ArducamEvkSDK & arducam_rgbir_remosaic. moved config file to appropriate location.
    
    * added function comments. changed data flow for clarity/simplicity.
    
    * unreachable code fixed. optimized demosaic function slightly

---
+++ modules/camera/camera_arducamir.py
@@ -0,0 +1,113 @@
+"""
+ArducamIR implementation of the camera wrapper.
+"""
+
+import enum
+import numpy as np
+import cv2
+
+import ArducamEvkSDK
+import arducam_rgbir_remosaic
+
+from . import base_camera
+
+CAMERA_CONFIG_DIR = "./config/camera_config.cfg"
+
+
+class ArducamOutput(enum.Enum):
+    """
+    Enum for ArducamIR output
+    """
+
+    RGB = 0
+    IR = 1
+
+
+class CameraArducamIR(base_camera.BaseCameraDevice):
+    """
+    Class for the ArducamSDK implementation of the ArducamIR camera.
+    """
+
+    __create_key = object()
+
+    @classmethod
+    def create(
+        cls, width: int, height: int, config: None
+    ) -> "tuple[True, CameraArducamIR] | tuple[False, None]":
+
+        camera = ArducamEvkSDK.Camera()
+
+        param = ArducamEvkSDK.Param()
+        param.config_file_name = CAMERA_CONFIG_DIR
+
+        if not camera.open(param):
+            return False, None
+
+        return True, CameraArducamIR(cls.__create_key, camera)
+
+    def __init__(
+        self,
+        class_private_create_key: object,
+        camera: ArducamEvkSDK.Camera,
+    ) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is CameraArducamIR.__create_key, "Use create() method."
+
+        self.__camera = camera
+        self.__camera.init()
+        self.__camera.start()
+
+    def __del__(self) -> None:
+        """
+        Destructor. Release hardware resources.
+        """
+        self.__camera.stop()
+        self.__camera.close()
+
+    def run(self) -> tuple[True, ArducamEvkSDK.Frame] | tuple[False, None]:
+        """
+        Takes a picture with ArducamIR camera.
+
+        Return: Success, Frame
+        """
+        image_data = self.__camera.capture()
+        if image_data is None:
+            return False, None
+
+        return True, image_data
+
+    def demosaic(self, image: ArducamEvkSDK.Frame, output: ArducamOutput) -> np.ndarray | None:
+        """
+        Converts Bayer Pattern & IR data to OpenCV Matrix
+        """
+        # Convert sensor data to useable format
+        data = self.format(image)
+        # Splits raw sensor data into bayer data and IR data using GRIG (Green, Red, IR, Green) filter pattern
+        bayer, ir = arducam_rgbir_remosaic.rgbir_remosaic(data, arducam_rgbir_remosaic.GRIG)
+        if output == ArducamOutput.RGB:
+            # Converts Bayer data to BGRA (Blue, Green, Red, Alpha)
+            return cv2.cvtColor(bayer, cv2.COLOR_BayerRG2BGRA)
+        # Converts IR data to BGRA
+        ir_color = cv2.cvtColor(ir, cv2.COLOR_GRAY2BGRA)
+        # Resize the IR image so that they are both the same size
+        return cv2.resize(ir_color, (bayer.shape[1], bayer.shape[0]))
+
+    def format(self, image: ArducamEvkSDK.Frame) -> np.ndarray:
+        """
+        Formats byte buffer sensor input into 8-bit arrays
+        """
+        width = image.format.width
+        height = image.format.height
+        bit_depth = image.format.bit_depth
+        data = image.data
+
+        if bit_depth > 8:
+            data = np.frombuffer(data, np.uint16).reshape(height, width)
+            # Reduce higher precision inputs to 8-bit arrays
+            data = (data >> (bit_depth - 8)).astype(np.uint8)
+        else:
+            data = np.frombuffer(data, np.uint8).reshape(height, width)
+
+        return data
---
+++ tests/integration/test_camera_arducamir.py
@@ -0,0 +1,64 @@
+"""
+Test ArducamIR camera physically and verifies configuration
+"""
+
+import time
+import cv2
+
+from modules.camera import camera_factory
+from modules.camera import camera_arducamir
+
+
+def main() -> int:
+    """
+    Main function
+    """
+
+    result, device = camera_factory.create_camera(
+        camera_factory.CameraOption.ARDUCAMIR,
+        100,
+        200,
+        None,
+    )
+    if not result:
+        print("ArducamIR camera creation error.")
+        return -1
+
+    while True:
+        result, image = device.run()
+        if not result:
+            print("ERROR, image not captured.\n")
+            continue
+
+        if image.seq % 50 == 0:  # Avoiding too many prints, 50 is an arbitrary number
+            print("Timestamp: [{0:s}]", time.ctime(float(image.timestamp / 10**3)))
+            print("Bit Depth: {0:2d}", image.format.bit_depth)
+            print("Format Code: {0:2d}", image.format.format_code)
+            print("Height: {0:3d}", image.format.height)
+            print("Width: {0:3d}\n", image.format.width)
+
+            # Assertions for Arducam cfg config values
+            assert image.format.bit_depth == 12
+            assert image.format.format_code == 2307
+            assert image.format.width == 640
+            assert image.format.height == 480
+
+        color_frame = device.demosaic(image, camera_arducamir.ArducamOutput.IR)
+
+        if image.data is not None:
+            cv2.imshow("ArducamIR Test", color_frame)
+        cv2.setWindowTitle("ArducamIR Test", "ArducamIR Test " + str(image.seq))
+
+        # 1ms delay between next picture
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    return 0
+
+
+if __name__ == "__main__":
+    result_main = main()
+    if result_main != 0:
+        print(f"ERROR: Status code: {result_main}")
+
+    print("Done!")
