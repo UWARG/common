14dae3875b4594178a2da6eeccb9354b7ba79ad3
Andy Dai <andydai314@gmail.com>
2025-07-24 13:49:44 -0400

    HITL camera emulator module (#86)
    * implemented camera emulator with integration test
    
    * use os.path to join test images folder path + formatting
    
    * disabled pylint catch for broad exceptions
    
    * added package requirements
    
    * add error checks to virtual camera initialization
    
    ---------
    
    Co-authored-by: Andy Dai <andydai602@gmail.com>

--- modules/hitl/camera_emulator.py
+++ modules/hitl/camera_emulator.py
@@ -1,6 +1,17 @@
 """
 Emulates camera input to PI.
+
+Requires OBS Virtual Camera on Windows or
+v4l2loopback for Linux to be installed to work
 """
+
+import os
+import pyvirtualcam
+import cv2
+
+IMAGE_SIZE = (720, 480)
+IMAGE_FORMATS = (".png", ".jpeg", "jpg")
+CAMERA_FPS = 30
 
 
 class CameraEmulator:
@@ -25,12 +36,101 @@ class CameraEmulator:
         if not isinstance(images_path, str):
             return False, None
 
-        return True, CameraEmulator(cls.__create_key, images_path)
+        try:
+            virtual_camera_instance = pyvirtualcam.Camera(IMAGE_SIZE[0], IMAGE_SIZE[1], CAMERA_FPS)
 
-    def __init__(self, class_private_create_key: object, images_path: str) -> None:
+        # Required for catching library exceptions
+        # pylint: disable-next=broad-exception-caught
+        except Exception as e:
+            print(
+                "Error creating virtual camera (Check if OBS or v4l2loopback is installed): "
+                + str(e)
+            )
+            return False, None
+
+        if virtual_camera_instance is None:
+            return False, None
+
+        return True, CameraEmulator(cls.__create_key, images_path, virtual_camera_instance)
+
+    def __init__(
+        self, class_private_create_key: object, images_path: str, virtual_camera: pyvirtualcam
+    ) -> None:
         """
         Private constructor, use create() method.
         """
         assert class_private_create_key is CameraEmulator.__create_key, "Use create() method"
 
-        self._images_path = images_path
+        self.__image_folder_path = images_path
+        self.__virtual_camera = virtual_camera
+        self.__image_paths: "list[str]" = []
+        self.__current_frame = None
+        self.__image_index = 0
+
+        self.__get_images()
+        self.update_current_image()
+
+    def send_frame(self) -> None:
+        """
+        sends a new frame to virtual camera, should be called in a loop
+
+        """
+        try:
+            self.__virtual_camera.send(self.__current_frame)
+
+        # Required for catching library exceptions
+        # pylint: disable-next=broad-exception-caught
+        except Exception as e:
+            print("Cannot send frame" + str(e))
+
+    def sleep_until_next_frame(self) -> None:
+        """
+        Waits an amount of time to maintain targeted framerate
+        (Wrapper for pyvirtualcam)
+        """
+        self.__virtual_camera.sleep_until_next_frame()
+
+    def update_current_image(self) -> None:
+        """
+        sets curr_img to the image specified by the curr_img_index
+        """
+
+        has_image = False
+        loop_count = 0
+
+        # loop to skip image if read fails
+        while not has_image and loop_count < len(self.__image_paths):
+            try:
+                image_path = self.__image_paths[self.__image_index]
+                image = cv2.imread(image_path)
+                self.__current_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                has_image = True
+
+            # Required for catching library exceptions
+            # pylint: disable-next=broad-exception-caught
+            except Exception as e:
+                print("Could not read image: " + image_path + " Error: " + str(e))
+                self.next_image()
+                loop_count += 1
+
+    def next_image(self) -> None:
+        """
+        increments image index by 1
+        """
+
+        self.__image_index = (self.__image_index + 1) % len(self.__image_paths)
+
+    def __get_images(self) -> None:
+        """
+        populates _images array with paths of all images in the folder
+        """
+        try:
+            for image in os.listdir(self.__image_folder_path):
+                if image.endswith(IMAGE_FORMATS):
+                    path = os.path.join(self.__image_folder_path, image)
+                    self.__image_paths.append(path)
+
+        # Required for catching library exceptions
+        # pylint: disable-next=broad-exception-caught
+        except Exception as e:
+            print("Error reading images: " + str(e))
---
+++ tests/integration/camera_emulator/test_camera_emulator.py
@@ -0,0 +1,38 @@
+"""
+Test for the hitl camera emulator
+
+INSTRUCTIONS:
+- Using a windows environemnt with OBS installed,
+run this script (this will create a virtual camera and start streaming images to it)
+
+- Run test_camera_read (will open the virtual camera using opencv and display the images)
+Note: may need to mess around with camera index 
+(usually will be # of cameras connected to computer + 1)
+"""
+
+import time
+import os
+from modules.hitl.camera_emulator import CameraEmulator
+
+
+def main() -> None:
+    """
+    Runs camera emulator
+    """
+    images_folder_path = os.path.join("tests", "integration", "camera_emulator", "images")
+    has_cam, cam_emulator = CameraEmulator.create(images_folder_path)
+    if has_cam:
+        print("CAMERA EMULATOR RUNNING")
+        start = time.perf_counter()
+        while True:
+            cam_emulator.send_frame()
+            cam_emulator.sleep_until_next_frame()
+            if time.perf_counter() - start > 1:
+                cam_emulator.next_image()
+                cam_emulator.update_current_image()
+                start = time.perf_counter()
+
+
+if __name__ == "__main__":
+    main()
+    print("CAMERA EMULATOR STOPPED")
