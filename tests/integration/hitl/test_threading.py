f6a9023e7bb7b55306258b28eeb646197ea7206f
honzikschenk <nojschenk@gmail.com>
2025-09-17 23:56:01 -0400

    Actually removes camera from test

--- tests/integration/hitl/test_threading.py
+++ tests/integration/hitl/test_threading.py
@@ -18,7 +18,7 @@ def main() -> int:
     images_folder_path = os.path.join("tests", "integration", "camera_emulator", "images")
 
     result, controller = flight_controller.FlightController.create(
-        PIXHAWK_ADDRESS, 57600, True, True, False # True, images_folder_path
+        PIXHAWK_ADDRESS, 57600, True, True # True, images_folder_path
     )
     if not result:
         print("Failed to create flight controller")
